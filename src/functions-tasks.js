9230e9c09869de4daf17d917bd55d2fe174915e4
ezoza <ezozakhudayberdieva@mail.ru>
2024-04-12 17:57:02 +0500

    feat: add partialUsingArguments function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -174,8 +174,11 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const result = (...arg) => {
+    return fn(...args1, ...arg);
+  };
+  return result;
 }
 
 /**
